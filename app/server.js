99441dcce53f9d1879d29db66c95fc5f61488ba7
Antonio Guimaraes <acgfbr@gmail.com>
2020-09-07 13:44:35 -0300

    config port heroku

--- app/server.js
+++ app/server.js
@@ -51,7 +51,7 @@ app.use(function (req, res) {
 });
 
 
-const PORT = process.env.PORT || config.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log('----------------------------');
